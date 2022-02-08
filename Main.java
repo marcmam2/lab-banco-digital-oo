9afbd61b5fa2e5af7ec25297a255a71216db2c5a
Marcos Torres <marcmam2@gmail.com>
2022-02-08 06:08:28 +0000

    adição de operação no método main, exemplificando caso de saldo insuficiente

--- Main.java
+++ Main.java
@@ -8,7 +8,15 @@ public class Main {
 		Conta cc = new ContaCorrente(venilton);
 		Conta poupanca = new ContaPoupanca(venilton);
 
-		cc.depositar(100);
+		cc.depositar(10);
+		cc.transferir(100, poupanca);
+		
+		cc.imprimirExtrato();
+		poupanca.imprimirExtrato();
+
+    System.out.println("\n\n");
+
+    cc.depositar(90);
 		cc.transferir(100, poupanca);
 		
 		cc.imprimirExtrato();
